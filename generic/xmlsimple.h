bb0c714ce0d1cb69bbeb003e8079e750fb448e00
rolf <rolf>
2002-06-23 00:21:25 +0000

    Made simple parser -baseurl and -externalentitycommand aware.

--- generic/xmlsimple.h
+++ generic/xmlsimple.h
@@ -1,3 +1,4 @@
 
 domDocument *  XML_SimpleParseDocument ( char *xml, int ignoreWhiteSpaces, 
+                                         char *baseURI, Tcl_Obj *extResolver,
                                          int *pos, char **errStr );
