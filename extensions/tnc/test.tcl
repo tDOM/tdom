34fc75f0d98c978ffe25ea2821e3e850c268145c
rolf <rolf>
2002-10-30 00:53:42 +0000

    Arrg. Removed now superfluous.

--- extensions/tnc/test.tcl
+++ extensions/tnc/test.tcl
@@ -6,15 +6,6 @@ if {[llength $argv] != 1} {
     exit 1
 }
 
-switch [file pathtype [lindex $argv 0]] {
-    "relative" {
-        set baseurl "file://[pwd]/[lindex $argv 0]"
-    }
-    default {
-        set baseurl "file://[lindex $argv 0]"
-    }
-}
-
 set parser [expat \
         -baseurl [tDOM::baseURL $argv]                          \
         -externalentitycommand tdom::extRefHandler              \
