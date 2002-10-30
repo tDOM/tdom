c1459acb3f9a0ec302176dd510d1c11b6de992c0
rolf <rolf>
2002-10-30 00:52:21 +0000

    Preliminary update.

--- extensions/tnc/test.tcl
+++ extensions/tnc/test.tcl
@@ -1,26 +1,9 @@
-load [file dirname [info script]]/../../unix/tdom0.6.so
-load [file dirname [info script]]/tnc0.1.so
 
+package require tnc
 
-#Yes, yes, this should be use tcllib's uri module or something
-#similar..
-proc externalEntityRefHandler {base systemId publicId} {
-    if {[regexp {^[a-zA-Z]+:/} $systemId]}  {
-        # Seems to be not relative to the base
-        if {[regexp { *file:(.*)} $systemId dummy path]} {
-            return [list filename $systemId $path]
-        } else {
-            return -code error  -errorinfo "externalEntityRefHandler: can only handle file URL's"
-        }
-    } else {
-        if {[regexp { *file:(.*)} $base dummy basepath]} {
-            set basedir [file dirname $basepath]
-            set entitypath "${basedir}/${systemId}"
-            return [list filename "file://${basedir}/${systemId}" $entitypath]
-        } else {
-            return -code error  -errorinfo "externalEntityRefHandler: can only handle file URL's"
-        }
-    }
+if {[llength $argv] != 1} {
+    puts stderr "usage: $argv0 <xml-file>"
+    exit 1
 }
 
 switch [file pathtype [lindex $argv 0]] {
@@ -33,11 +16,11 @@ switch [file pathtype [lindex $argv 0]] {
 }
 
 set parser [expat \
-        -baseurl $baseurl                                       \
-        -externalentitycommand externalEntityRefHandler         \
+        -baseurl [tDOM::baseURL $argv]                          \
+        -externalentitycommand tdom::extRefHandler              \
         -paramentityparsing notstandalone]
 
 tnc $parser enable
 
-$parser parsefile [lindex $argv 0]
+$parser parsefile $argv
 
