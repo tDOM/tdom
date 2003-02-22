73f3915240d0b5cee98a2cf431dd72d6c2b7dd1a
rolf <rolf>
2003-02-22 21:06:46 +0000

    Added new tnc method getValidateCmd. Changed to use the same memory allocator/free defines as the tDOM core. Bumped the version to 0.3.

--- extensions/tnc/test.tcl
+++ extensions/tnc/test.tcl
@@ -1,5 +1,14 @@
 
-package require tnc
+if {[catch {package require tnc 0.3} errMsg]} {
+    if {[catch {load ./libtnc0.3.0.so}]} {
+        puts $errMsg
+        exit 1
+    }
+}
+if {[lsearch [namespace children] ::tDOM] == -1} {
+    # tcldomsh without the script library. Source the lib.
+    source [file join [file dir [info script]] ../lib tdom.tcl]
+}
 
 if {[llength $argv] != 1} {
     puts stderr "usage: $argv0 <xml-file>"
@@ -8,7 +17,7 @@ if {[llength $argv] != 1} {
 
 set parser [expat \
         -baseurl [tDOM::baseURL $argv]                          \
-        -externalentitycommand tdom::extRefHandler              \
+        -externalentitycommand tDOM::extRefHandler              \
         -paramentityparsing notstandalone]
 
 tnc $parser enable
