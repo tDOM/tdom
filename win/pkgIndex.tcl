d18a53b392c9031250bfbd7e2b2da808bb03342e
rolf <rolf>
2003-03-22 02:11:51 +0000

    Updated to new version 0.7.6.

--- win/pkgIndex.tcl
+++ win/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # tDOM Tcl package index file
 
-package ifneeded tdom 0.7        \
+package ifneeded tdom 0.7.6       \
     "source [file join $dir tdom.tcl]; \
-     load   [file join $dir tdom07[info sharedlibextension] ] tdom  "
+     load   [file join $dir libtdom076[info sharedlibextension] ] tdom  "
