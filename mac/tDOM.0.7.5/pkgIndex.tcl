978e3f422704ec2554344ff303c2bc00fdfe98fd
rolf <rolf>
2003-03-23 01:16:36 +0000

    Removed in favor of a version-less tDOM output directory.

--- mac/tDOM.0.7.5/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-if {[catch {package require Tcl 8.2}]} return
-
-package ifneeded tdom 0.7.5 [list load [file join $dir tDOM.0.7.5.dylib] Tdom]
-
-
-package ifneeded tnc 0.2.0 "package require tdom;
-load [list [file join $dir tnc.0.2.0.dylib]] tnc"
