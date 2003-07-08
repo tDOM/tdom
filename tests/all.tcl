85e8ed2273a6a24e6500a3d55c13ad210e8b5804
rolf <rolf>
2003-07-08 16:00:46 +0000

    Added contraint need_uri, so that the few tests, which needs the tcllib uri package are skipped, if the uri package isn't present.

--- tests/all.tcl
+++ tests/all.tcl
@@ -16,8 +16,15 @@ if {$tcl_version >= 8.1} {
             ::tcltest::testConstraint] == -1} {
         set ::tcltest::testConfig(need_i18n) 1
         set ::tcltest::testConstraints(need_i18n) 1
+        if {[info procs ::tDOM::extRefHandler] ne ""} {
+            set ::tcltest::testConfig(need_uri) 1
+            set ::tcltest::testConstraints(need_uri) 1
+        }
    } else {
         ::tcltest::testConstraint need_i18n 1
+       if {[info procs ::tDOM::extRefHandler] ne ""} {
+           ::tcltest::testConstraint need_uri 1
+       }
     }
 }
 
