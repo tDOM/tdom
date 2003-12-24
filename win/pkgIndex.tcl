b64507af2cb557fa2e755ec35f5bf2107b9a5dd7
rolf <rolf>
2003-12-24 02:33:04 +0000

    Bug fix: Protect against spaces in the path to the package.

--- win/pkgIndex.tcl
+++ win/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # tDOM Tcl package index file
 
 package ifneeded tdom 0.8.0       \
-    "source [file join $dir tdom.tcl]; \
-     load   [file join $dir libtdom080[info sharedlibextension] ] tdom  "
+    "[list source [file join $dir tdom.tcl]]; \
+     [list load   [file join $dir libtdom080[info sharedlibextension] ] tdom]"
