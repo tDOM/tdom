b56745c4cdbc5a8ac75acad360c7dfe8f0578853
zv <zv>
2005-01-16 11:55:48 +0000

    Bumped version to 0.8.1

--- win/pkgIndex.tcl
+++ win/pkgIndex.tcl
@@ -1,5 +1,5 @@
 # tDOM Tcl package index file
 
-package ifneeded tdom 0.8.0       \
-    "[list load   [file join $dir libtdom080[info sharedlibextension] ] tdom];\
+package ifneeded tdom 0.8.1 \
+    "[list load   [file join $dir libtdom081[info sharedlibextension] ] tdom];\
      [list source [file join $dir tdom.tcl]]"
