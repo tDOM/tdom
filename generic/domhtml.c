956ffab84671f938179e628426da0acdcc5b1ee9
rolf <rolf>
2007-10-30 01:04:22 +0000

    ID Attributes in HTML documents parsed with -html do what you expect: you find it with getElementById.

--- generic/domhtml.c
+++ generic/domhtml.c
@@ -1315,7 +1315,22 @@ HTML_SimpleParse (
                 memmove(attrnode->nodeValue, ArgVal, nArgVal);
                 *(attrnode->nodeValue + nArgVal) = 0;
                 if (ampersandSeen) {
-                    TranslateEntityRefs(attrnode->nodeValue, &(attrnode->valueLength) );
+                    TranslateEntityRefs(attrnode->nodeValue, 
+                                        &(attrnode->valueLength) );
+                }
+                if (!strcmp(ArgName, "id")) {
+                    if (!doc->ids) {
+                        doc->ids = MALLOC (sizeof (Tcl_HashTable));
+                        Tcl_InitHashTable (doc->ids, TCL_STRING_KEYS);
+                    }
+                    h = Tcl_CreateHashEntry (doc->ids, attrnode->nodeValue,
+                                             &hnew);
+                    /* How to resolve in case of dublicates?  We
+                       follow, what the core dom building code does:
+                       the first value in document order wins. */
+                    if (hnew) {
+                        Tcl_SetHashValue (h, node);
+                    }
                 }
                 if (node->firstAttr) {
                     lastAttr->nextSibling = attrnode;
